905295bb82f9fecf9dfec5cd995cccc0287b3213
AndersonSantos07 <andersonfullstackdevoloper@gmail.com>
2024-06-19 19:34:33 -0300

    métodos específicados para o swagger

--- superpecas/src/main/java/br/com/masterclass/superpecas/controller/CarrosController.java
+++ superpecas/src/main/java/br/com/masterclass/superpecas/controller/CarrosController.java
@@ -2,6 +2,8 @@ package br.com.masterclass.superpecas.controller;
 
 import br.com.masterclass.superpecas.dto.*;
 import br.com.masterclass.superpecas.model.CarrosModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -14,34 +16,40 @@ import java.util.List;
 @RestController
 @RequestMapping("/carro")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@Tag(name = "Api parte de Carros", description = "Api Carros" )
 public class CarrosController {
 
     @Autowired
     CarrosService service;
 
     @GetMapping("/listaTop10Fabricantes")
+    @Operation(summary = "Retorna uma lista json.", description = "Retorna uma lista json com top dez Fabricantes com mais carros.")
     public ResponseEntity<List<FabricanteTotalCarrosDTO>> getTop10Fabricantes(){
         return ResponseEntity.status(HttpStatus.OK).body(service.listarTopFabricantes());
     }
 
     @GetMapping("/listaTop10CarroComMaisPecas")
+    @Operation(summary = "Retorna uma lista json.", description = "Retorna uma lista json com top dez Carros com mais peças.")
     public ResponseEntity<List<CarrosPecasDTO>> getTop10CarrosComMaisPecas(){
         return ResponseEntity.status(HttpStatus.OK).body(service.listarTop10CarrosComMaisPecas());
     }
 
     @GetMapping("/listaTodos")
+    @Operation(summary = "Retorna uma lista json.", description = "Retorna uma lista json de todos os carros.")
     public ResponseEntity<List<CarrosModel>> getListarTodos(){
 
         return ResponseEntity.status(HttpStatus.OK).body(service.listarTodos());
     }
 
     @GetMapping("/listaTodosPaginado")
+    @Operation(summary = "Retorna uma lista json.", description = "Retorna uma lista json de Carros paginado, cada página contém dez Carros.")
     public ResponseEntity<Page<CarrosModel>> getListarTodosPaginado(@RequestParam(defaultValue = "0") int page,
                                                                  @RequestParam(defaultValue = "10") int size){
         return ResponseEntity.status(HttpStatus.OK).body(service.listarTodosPaginados(page, size));
     }
 
     @GetMapping("listaTodosPaginado/{termo}")
+    @Operation(summary = "Retorna uma lista json.", description = "Retorna uma lista json paginado dependendo do termo(filtro) enviado pelo usuário, cada página contém dez Carros.")
     public ResponseEntity<Page<CarrosModel>> getListarTodosPaginadosPorTermo(@RequestParam(defaultValue = "0") int page,
                                                                           @RequestParam(defaultValue = "10") int size,
                                                                           @PathVariable String termo){
@@ -49,28 +57,33 @@ public class CarrosController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retorna um item em json.", description = "Retorna um item Carro em json dependendo do id passado pelo usuário.")
     public ResponseEntity<CarrosModel> getPorId(@PathVariable int id){
         return ResponseEntity.status(HttpStatus.OK).body(service.procurarPorId(id));
     }
 
     @GetMapping("/listaTodosFabricantes")
+    @Operation(summary = "Retorna uma lista json.", description = "Retorna uma lista json com todos os fabricantes.")
     public ResponseEntity<List<FabricanteDTO>> getTodosFabricantes(){
         return ResponseEntity.status(HttpStatus.OK).body(service.listarTodosFabricantes());
     }
 
     @PostMapping
+    @Operation(summary = "Criar novo carro.", description = "Método para criar um novo Carro.")
     public ResponseEntity<String> postNovoCarro(@RequestBody CarroCriarDTO carroDTO){
         this.service.criarNovoCarro(carroDTO);
         return  ResponseEntity.status(HttpStatus.CREATED).body("Carro gravado com sucesso!");
     }
 
     @PutMapping
+    @Operation(summary = "Atualizar o Carro.", description = "Método para atualizar um carro especifíco.")
     public ResponseEntity<String> putAtualizarCarro(@RequestBody CarroAtualizarDTO carroDTO){
         this.service.atualizarCarro(carroDTO);
         return ResponseEntity.status(HttpStatus.OK).body("Carro Atualizado com Sucesso!");
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar um carro pelo identificador.", description = "Método para deletar o carro por id.")
     public ResponseEntity<String> deleteCarro(@PathVariable int id){
         this.service.excluirCarro(id);
         return ResponseEntity.status(HttpStatus.OK).body("Carro Excluído com Sucesso! ");
--- superpecas/src/main/java/br/com/masterclass/superpecas/controller/PecasController.java
+++ superpecas/src/main/java/br/com/masterclass/superpecas/controller/PecasController.java
@@ -4,6 +4,8 @@ import br.com.masterclass.superpecas.dto.PecaAtualizarDTO;
 import br.com.masterclass.superpecas.dto.PecaCriarDTO;
 import br.com.masterclass.superpecas.model.PecasModel;
 import br.com.masterclass.superpecas.service.PecasService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -14,18 +16,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/peca")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@Tag(name = "Api parte de Peças", description = "Api de Peças")
 public class PecasController {
 
     @Autowired
     private PecasService service;
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retorna um objeto json.", description = "Retorna um objeto json, dependendo do ID que é enviado.")
     public ResponseEntity<PecasModel> getPorId(@PathVariable int id){
 
         return ResponseEntity.status(HttpStatus.OK).body(service.buscarPorId(id));
     }
 
     @GetMapping("/listaTodosPaginado")
+    @Operation(summary = "Retorna uma lista json de objetos paginados.", description = "Retorna uma lista de objetos paginados, cada página contém dez itens.")
     public ResponseEntity<Page<PecasModel>> listarTodosPaginado(@RequestParam(defaultValue = "0") int page,
                                                                 @RequestParam(defaultValue = "10") int size){
 
@@ -33,6 +38,7 @@ public class PecasController {
     }
 
     @GetMapping("listaTodosPaginado/{termo}")
+    @Operation(summary = "Retorna uma lista json de objetos paginados.", description = "Retorna uma lista json de objetos paginados dependendo do termo que é enviado como filtro, cada página contém dez itens.")
     public ResponseEntity<Page<PecasModel>> listarTodosPaginadoPorTermo(@RequestParam(defaultValue = "0") int page,
                                                                         @RequestParam(defaultValue = "10") int size,
                                                                         @PathVariable String termo){
@@ -41,18 +47,21 @@ public class PecasController {
 
 
     @PostMapping
+    @Operation(summary = "Criar peça.", description = "Método para criar a peça.")
     public ResponseEntity<String> criarPeca(@RequestBody PecaCriarDTO pecaDTO){
         this.service.criarPeca(pecaDTO);
         return ResponseEntity.status(HttpStatus.OK).body("Peça gravada com sucesso! ");
     }
 
     @PutMapping
+    @Operation(summary = "Atualizar a peça", description = "Método para atualizar uma peça específica.")
     public ResponseEntity<String> atualizarPeca(@RequestBody PecaAtualizarDTO pecaDTO){
         service.atualizarPeca(pecaDTO);
         return ResponseEntity.status(HttpStatus.OK).body("Peça atualizada com sucesso! ");
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir Peça", description = "Método para excluir a peça, dependendo do ID que é passado.")
     public ResponseEntity<String> excluirPeca(@PathVariable int id){
         service.excluirPeca(id);
         return ResponseEntity.status(HttpStatus.OK).body("Peça excluída com sucesso! ");
